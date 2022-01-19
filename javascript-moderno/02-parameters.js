77c9c3aa8e99da7db13df3248c4d3d1be83942e0
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-01-19 04:41:37 -0300

    aula 05.02: parametros default em funçoes

---
+++ javascript-moderno/02-parameters.js
@@ -0,0 +1,6 @@
+function printGreetings(message = 'Mensagem padrão') {
+  console.log(message);
+}
+
+printGreetings(); // opt: Mensagem padrão
+printGreetings('Seja muito bem-vinde!'); // opt: 'Seja muito bem-vinde!'
