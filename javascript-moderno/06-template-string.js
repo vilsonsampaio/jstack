901bf9f1c1cd0a405a59678d5f03054667148432
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-01-25 15:02:15 -0300

    aula 05.06: template strings

---
+++ javascript-moderno/06-template-string.js
@@ -0,0 +1,11 @@
+// Permite quebra de linhas
+console.log(`Esta é a primeira linha
+Esta é a segunda linha
+`);
+
+
+// Interpolação de strings
+const user = 'Mateus';
+
+console.log(`Seja bem-vindo, ${user}!`);
+console.log("Seja bem vindo, " + user + "!"); // Caminho padrão (concatenação)
