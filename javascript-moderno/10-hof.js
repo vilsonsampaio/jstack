6d4cef94665cfe91e927fa48c5751e79cb5bcfa8
Vilson Sampaio <vilson.sampaio04@gmail.com>
2022-06-06 14:44:18 -0300

    aula 05.11: HOF (.some e .every)

--- javascript-moderno/10-hof.js
+++ javascript-moderno/10-hof.js
@@ -1,10 +1,14 @@
 const products = [
   { name: 'iPhone', price: 5000, quantity: 2 },
   { name: 'MacBook Pro', price: 20000, quantity: 1 },
-  { name: 'Magic Mouse', price: 1000, quantity: 5 },
+  { name: 'Magic Mouse', price: 1001, quantity: 5 },
 ];
 
 // .find e .findIndex
 const iPhone = products.find(product => product.name === 'iPhone');
 const iPhonePosition = products.findIndex(product => product.name === 'iPhone');
-console.log({ iPhone, iPhonePosition });
+
+
+// .some e .every
+const isSomeProductBelow1000 = products.some(product => product.price < 1000);
+const isEveryProductsOver1000 = products.every(product => product.price > 1000);
